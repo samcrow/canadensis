80e6a6aa313edff52240787ea2583d1656d5343a
Sam Crow <scrow@eng.ucsd.edu>
2021-05-16 16:04:27 -0700

    Added free functions for bxCAN tasks

--- canadensis_bxcan/src/lib.rs
+++ canadensis_bxcan/src/lib.rs
@@ -22,9 +22,7 @@ macro_rules! debugln {
     ($fmt:expr, $($arg:tt)*) => {};
 }
 
-mod pnp;
-
-pub use crate::pnp::BxCanPnpClient;
+pub mod pnp;
 
 use bxcan::filter::{BankConfig, Mask32};
 use bxcan::{Can, ExtendedId, FilterOwner, Instance, Mailbox};
@@ -32,7 +30,7 @@ use canadensis::{Node, TransferHandler};
 use canadensis_can::queue::FrameQueueSource;
 use canadensis_can::OutOfMemoryError;
 use canadensis_core::time::{Clock, Instant};
-use canadensis_filter_config::optimize;
+use canadensis_filter_config::{optimize, Filter};
 use core::cmp::Ordering;
 use core::convert::{Infallible, TryFrom};
 
@@ -72,23 +70,7 @@ where
     where
         C: FilterOwner,
     {
-        // Get ideal filters
-        let mut filters = self.node.frame_filters()?;
-        // Reduce the filters if necessary
-        let mut hardware_filters = self.can.modify_filters();
-        let max_hardware_filters = hardware_filters.num_banks();
-        let optimized_filters = optimize(&mut filters, max_hardware_filters.into());
-        // Apply filters
-        hardware_filters.clear();
-        for (i, filter) in optimized_filters.iter().enumerate() {
-            let id = ExtendedId::new(filter.id()).unwrap();
-            let mask = ExtendedId::new(filter.mask()).unwrap();
-            hardware_filters.enable_bank(
-                i as u8,
-                BankConfig::Mask32(Mask32::frames_with_ext_id(id, mask)),
-            );
-        }
-        Ok(())
+        configure_node_filters(&self.node, &mut self.can)
     }
 
     /// Receives all incoming CAN frames from the CAN peripheral, converts them into transfers,
@@ -124,94 +106,137 @@ where
     /// This function returns a WouldBlock error if frames are waiting to be transmitted
     /// but no suitable transmit mailbox is open.
     pub fn send_frames(&mut self) -> nb::Result<(), Infallible> {
-        let now = self.node.clock_mut().now();
-        self.clean_expired_frames(now);
-        while let Some(frame) = self.node.frame_queue_mut().pop_frame() {
-            // Check that the frame's deadline has not passed
-            match frame.timestamp().overflow_safe_compare(&now) {
-                Ordering::Greater | Ordering::Equal => {
-                    // Deadline is now or in the future. Continue to transmit.
-                    let send_status = self.send_frame(frame);
-                    match send_status {
-                        Ok(()) => {}
-                        Err(nb::Error::Other(infallible)) => match infallible {},
-                        Err(nb::Error::WouldBlock) => {
-                            // The self.send_frame call already put the frame back in the queue
-                            return Err(nb::Error::WouldBlock);
-                        }
+        send_frames(&mut self.node, &mut self.can, &mut self.deadlines)
+    }
+}
+
+/// Configures filters on a CAN peripheral to accept all frames that the provided node is subscribed
+/// to
+pub fn configure_node_filters<N, I>(node: &N, can: &mut Can<I>) -> Result<(), OutOfMemoryError>
+where
+    N: Node,
+    I: Instance + FilterOwner,
+{
+    let mut filters = node.frame_filters()?;
+    optimize_and_apply_filters(&mut filters, can);
+    Ok(())
+}
+
+/// Sends frames from the node's outgoing frame queue onto the CAN bus
+///
+/// This function also discards any frames that have not been transmitted by their deadlines.
+///
+/// This function returns a WouldBlock error if frames are waiting to be transmitted
+/// but no suitable transmit mailbox is open.
+pub fn send_frames<N, C>(
+    node: &mut N,
+    can: &mut Can<C>,
+    deadlines: &mut DeadlineTracker<N::Instant>,
+) -> nb::Result<(), Infallible>
+where
+    N: Node,
+    N::FrameQueue: FrameQueueSource<N::Instant>,
+    C: Instance,
+{
+    let now = node.clock_mut().now();
+    clean_expired_frames(deadlines, can, now);
+    while let Some(frame) = node.frame_queue_mut().pop_frame() {
+        // Check that the frame's deadline has not passed
+        match frame.timestamp().overflow_safe_compare(&now) {
+            Ordering::Greater | Ordering::Equal => {
+                // Deadline is now or in the future. Continue to transmit.
+                let send_status = send_frame(node, can, deadlines, frame);
+                match send_status {
+                    Ok(()) => {}
+                    Err(nb::Error::Other(infallible)) => match infallible {},
+                    Err(nb::Error::WouldBlock) => {
+                        // The self.send_frame call already put the frame back in the queue
+                        return Err(nb::Error::WouldBlock);
                     }
                 }
-                Ordering::Less => {
-                    // Deadline passed, ignore frame
-                    drop(frame);
-                }
+            }
+            Ordering::Less => {
+                // Deadline passed, ignore frame
+                drop(frame);
             }
         }
-        // All frames in the queue processed
-        Ok(())
     }
+    // All frames in the queue processed
+    Ok(())
+}
 
-    /// Puts one frame in a transmit mailbox to be sent
-    ///
-    /// If all mailboxes are full with frames of equal or greater priority, this function returns
-    /// the frame to the outgoing frame queue and returns a WouldBlock error.
-    fn send_frame(
-        &mut self,
-        frame: canadensis_can::Frame<N::Instant>,
-    ) -> nb::Result<(), Infallible> {
-        // Convert frame to BXCAN format
-        let bxcan_frame = uavcan_frame_to_bxcan(&frame);
-        match self.can.transmit_and_get_mailbox(&bxcan_frame) {
-            Ok((None, mailbox)) => {
-                // Store the deadline for the frame just submitted
-                let _ = self.deadlines.replace(mailbox, frame.timestamp());
-                Ok(())
-            }
-            Ok((Some(removed_frame), mailbox)) => {
-                // Store the deadline for the frame just submitted, and get the deadline for
-                // the removed frame
-                let removed_frame_deadline = self
-                    .deadlines
-                    .replace(mailbox, frame.timestamp())
-                    .expect("Bug: removed a frame from the mailbox, but no deadline");
-                let removed_frame = bxcan_frame_to_uavcan(&removed_frame, removed_frame_deadline)
-                    .expect("Bug: Replaced frame has invalid format");
-                // Put the removed frame back in the queue to be transmitted later
-                // This may return an error if it runs out of memory, but there's nothing we can
-                // do about that.
-                let _ = self.node.frame_queue_mut().return_frame(removed_frame);
-                Ok(())
-            }
-            Err(nb::Error::WouldBlock) => {
-                // No mailbox available for this frame. Put it back.
-                // Ignore out of memory
-                let _ = self.node.frame_queue_mut().return_frame(frame);
-
-                Err(nb::Error::WouldBlock)
-            }
-            Err(nb::Error::Other(infallible)) => match infallible {},
+/// Puts one frame in a transmit mailbox to be sent
+///
+/// If all mailboxes are full with frames of equal or greater priority, this function returns
+/// the frame to the outgoing frame queue and returns a WouldBlock error.
+fn send_frame<N, C>(
+    node: &mut N,
+    can: &mut Can<C>,
+    deadlines: &mut DeadlineTracker<N::Instant>,
+    frame: canadensis_can::Frame<N::Instant>,
+) -> nb::Result<(), Infallible>
+where
+    N: Node,
+    N::FrameQueue: FrameQueueSource<N::Instant>,
+    C: Instance,
+{
+    // Convert frame to BXCAN format
+    let bxcan_frame = uavcan_frame_to_bxcan(&frame);
+    match can.transmit_and_get_mailbox(&bxcan_frame) {
+        Ok((None, mailbox)) => {
+            // Store the deadline for the frame just submitted
+            let _ = deadlines.replace(mailbox, frame.timestamp());
+            Ok(())
         }
-    }
+        Ok((Some(removed_frame), mailbox)) => {
+            // Store the deadline for the frame just submitted, and get the deadline for
+            // the removed frame
+            let removed_frame_deadline = deadlines
+                .replace(mailbox, frame.timestamp())
+                .expect("Bug: removed a frame from the mailbox, but no deadline");
+            let removed_frame = bxcan_frame_to_uavcan(&removed_frame, removed_frame_deadline)
+                .expect("Bug: Replaced frame has invalid format");
+            // Put the removed frame back in the queue to be transmitted later
+            // This may return an error if it runs out of memory, but there's nothing we can
+            // do about that.
+            let _ = node.frame_queue_mut().return_frame(removed_frame);
+            Ok(())
+        }
+        Err(nb::Error::WouldBlock) => {
+            // No mailbox available for this frame. Put it back.
+            // Ignore out of memory
+            let _ = node.frame_queue_mut().return_frame(frame);
 
-    /// Aborts transmission for all frames placed in transmit mailboxes that have missed their
-    /// transmit deadlines
-    ///
-    /// now: The current time
-    fn clean_expired_frames(&mut self, now: N::Instant) {
-        for mailbox in [Mailbox::Mailbox0, Mailbox::Mailbox1, Mailbox::Mailbox2].iter() {
-            if let Some(deadline) = self.deadlines.get(mailbox.clone()) {
-                if now.overflow_safe_compare(&deadline) == Ordering::Greater {
-                    // Deadline has passed, abort transmission
-                    // Ignore if the mailbox is really empty or the frame has been transmitted.
-                    self.can.abort(mailbox.clone());
-                }
+            Err(nb::Error::WouldBlock)
+        }
+        Err(nb::Error::Other(infallible)) => match infallible {},
+    }
+}
+
+/// Aborts transmission for all frames placed in transmit mailboxes that have missed their
+/// transmit deadlines
+///
+/// now: The current time
+fn clean_expired_frames<I, C>(deadlines: &mut DeadlineTracker<I>, can: &mut Can<C>, now: I)
+where
+    I: Instant,
+    C: Instance,
+{
+    for mailbox in [Mailbox::Mailbox0, Mailbox::Mailbox1, Mailbox::Mailbox2].iter() {
+        if let Some(deadline) = deadlines.get(mailbox.clone()) {
+            if now.overflow_safe_compare(&deadline) == Ordering::Greater {
+                // Deadline has passed, abort transmission
+                // Ignore if the mailbox is really empty or the frame has been transmitted.
+                can.abort(mailbox.clone());
             }
         }
     }
 }
 
 /// Keeps track of the deadline for each frame in a CAN transmit mailbox
-struct DeadlineTracker<I> {
+///
+/// This struct does not have any public associated functions.
+pub struct DeadlineTracker<I> {
     deadlines: [Option<I>; 3],
 }
 
@@ -220,18 +245,18 @@ where
     I: Clone,
 {
     /// Creates a deadline tracker with no deadlines
-    pub fn new() -> Self {
+    pub(crate) fn new() -> Self {
         DeadlineTracker {
             deadlines: [None, None, None],
         }
     }
     /// Returns the deadline for a mailbox
-    pub fn get(&self, mailbox: Mailbox) -> Option<I> {
+    pub(crate) fn get(&self, mailbox: Mailbox) -> Option<I> {
         self.deadlines[mailbox as usize].clone()
     }
     /// Stores the deadline for a mailbox and returns the deadline for the previous frame in that
     /// mailbox, if any
-    pub fn replace(&mut self, mailbox: Mailbox, new_deadline: I) -> Option<I> {
+    pub(crate) fn replace(&mut self, mailbox: Mailbox, new_deadline: I) -> Option<I> {
         let slot = &mut self.deadlines[mailbox as usize];
         slot.replace(new_deadline)
     }
@@ -242,7 +267,7 @@ where
 /// # Panics
 ///
 /// This function panics if the provided frame has more than 8 bytes of data.
-fn uavcan_frame_to_bxcan<I>(frame: &canadensis_can::Frame<I>) -> bxcan::Frame {
+pub fn uavcan_frame_to_bxcan<I>(frame: &canadensis_can::Frame<I>) -> bxcan::Frame {
     let bxcan_id = bxcan::ExtendedId::new(frame.id().into()).unwrap();
     let bxcan_data = bxcan::Data::new(frame.data()).expect("Frame data more than 8 bytes");
     bxcan::Frame::new_data(bxcan_id, bxcan_data)
@@ -272,3 +297,24 @@ pub fn bxcan_frame_to_uavcan<I>(
 /// An error indicating that a frame did not have the correct format for use with UAVCAN
 #[derive(Debug)]
 pub struct InvalidFrameFormat;
+
+/// Optimizes the provided list and applies filters to a CAN peripheral
+fn optimize_and_apply_filters<I>(ideal_filters: &mut [Filter], can: &mut Can<I>)
+where
+    I: Instance + FilterOwner,
+{
+    // Reduce the filters if necessary
+    let mut hardware_filters = can.modify_filters();
+    let max_hardware_filters = hardware_filters.num_banks();
+    let optimized_filters = optimize(ideal_filters, max_hardware_filters.into());
+    // Apply filters
+    hardware_filters.clear();
+    for (i, filter) in optimized_filters.iter().enumerate() {
+        let id = ExtendedId::new(filter.id()).unwrap();
+        let mask = ExtendedId::new(filter.mask()).unwrap();
+        hardware_filters.enable_bank(
+            i as u8,
+            BankConfig::Mask32(Mask32::frames_with_ext_id(id, mask)),
+        );
+    }
+}
--- canadensis_bxcan/src/pnp.rs
+++ canadensis_bxcan/src/pnp.rs
@@ -1,9 +1,9 @@
-use bxcan::filter::{BankConfig, Mask32};
-use bxcan::{Can, ExtendedId, FilterOwner, Instance};
+use bxcan::{Can, FilterOwner, Instance};
 use canadensis_can::{Mtu, OutOfMemoryError};
 use canadensis_core::time::Clock;
 use canadensis_core::NodeId;
 use canadensis_pnp_client::{AllocationMessage, PnpClient};
+use core::convert::Infallible;
 
 /// A plug-and-play node ID assignment client that uses a bxCAN peripheral
 pub struct BxCanPnpClient<C: Clock, M, I: Instance> {
@@ -24,34 +24,15 @@ where
     /// Creates a node ID allocation client
     pub fn new(clock: C, mut can: Can<I>, unique_id: [u8; 16]) -> Result<Self, OutOfMemoryError> {
         let client = PnpClient::new(Mtu::Can8, unique_id)?;
-        {
-            // Configure the CAN filter to accept only node ID allocation messages
-            let mut filter_banks = can.modify_filters();
-
-            let mut filters = client.frame_fiters()?;
-            let optimized_filters =
-                canadensis_filter_config::optimize(&mut filters, filter_banks.num_banks().into());
-            filter_banks.clear();
-            for (i, filter) in optimized_filters.iter().enumerate() {
-                let id = ExtendedId::new(filter.id()).unwrap();
-                let mask = ExtendedId::new(filter.mask()).unwrap();
-                filter_banks.enable_bank(
-                    i as u8,
-                    BankConfig::Mask32(Mask32::frames_with_ext_id(id, mask)),
-                );
-            }
-        }
-
+        configure_pnp_filters(&client, &mut can)?;
         Ok(BxCanPnpClient { clock, can, client })
     }
 
     /// Publishes an ID allocation request, sending it onto the CAN bus
-    pub fn publish_request(&mut self) {
-        let now = self.clock.now();
-        let frame = self.client.assemble_request(now);
-        let bxcan_frame = crate::uavcan_frame_to_bxcan(&frame);
-        // Ignore errors. If there's a bus problem, we can't really do anything.
-        let _ = self.can.transmit(&bxcan_frame);
+    ///
+    /// This function returns an error if no transmit mailbox was available for the frame.
+    pub fn publish_request(&mut self) -> nb::Result<(), Infallible> {
+        publish_request(&mut self.clock, &mut self.client, &mut self.can)
     }
 
     /// Handles and parses incoming CAN frames, and returns a node ID if one was received
@@ -77,4 +58,37 @@ where
     pub fn free(self) -> (C, Can<I>) {
         (self.clock, self.can)
     }
+}
+
+/// Configures a CAN interface to accept only node ID allocation messages
+pub fn configure_pnp_filters<C, M, I>(
+    client: &PnpClient<C, M>,
+    can: &mut Can<I>,
+) -> Result<(), OutOfMemoryError>
+where
+    C: Clock,
+    M: AllocationMessage,
+    I: Instance + FilterOwner,
+{
+    let mut filters = client.frame_fiters()?;
+    crate::optimize_and_apply_filters(&mut filters, can);
+    Ok(())
+}
+
+/// Creates a node ID allocation request and sends it using the provided CAN interface
+pub fn publish_request<C, M, I>(
+    clock: &mut C,
+    client: &mut PnpClient<C, M>,
+    can: &mut Can<I>,
+) -> nb::Result<(), Infallible>
+where
+    C: Clock,
+    M: AllocationMessage,
+    I: Instance,
+{
+    let now = clock.now();
+    let frame = client.assemble_request(now);
+    let bxcan_frame = crate::uavcan_frame_to_bxcan(&frame);
+    can.transmit(&bxcan_frame)
+        .map(|displaced_frame| drop(displaced_frame))
 }
