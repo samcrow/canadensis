748485e4501ea36dc7042c366ae6d933848e3d86
Sam Crow <scrow@eng.ucsd.edu>
2023-02-20 10:58:21 -0800

    Added timestamp checks

--- canadensis/tests/can_loopback.rs
+++ canadensis/tests/can_loopback.rs
@@ -1,10 +1,9 @@
-//! Tests the Cyphal/CAN loopback behavior
+//! Tests the Cyphal/CAN loopback behavior by sending a loopback transfer and
 
 extern crate canadensis;
 extern crate canadensis_can;
 extern crate canadensis_data_types;
 extern crate canadensis_encoding;
-extern crate canadensis_linux;
 extern crate log;
 extern crate simplelog;
 
@@ -14,14 +13,14 @@ use canadensis::{Node, PublishToken, ResponseToken, TransferHandler};
 use canadensis_can::driver::{ReceiveDriver, TransmitDriver};
 use canadensis_can::{CanNodeId, CanReceiver, CanTransmitter, CanTransport, Frame, Mtu};
 use canadensis_core::subscription::Subscription;
-use canadensis_core::time::{milliseconds, Microseconds64};
+use canadensis_core::time::{milliseconds, Clock, Microseconds64};
 use canadensis_core::transfer::{MessageTransfer, ServiceTransfer, Transfer};
 use canadensis_core::{OutOfMemoryError, Priority};
 use canadensis_data_types::uavcan::time::synchronization_1_0::{self, Synchronization};
 use canadensis_encoding::Deserialize;
-use canadensis_linux::SystemClock;
 use log::LevelFilter;
 use simplelog::{ColorChoice, Config, TerminalMode};
+use std::cell::Cell;
 use std::collections::vec_deque::VecDeque;
 use std::convert::{Infallible, TryFrom};
 
@@ -35,9 +34,10 @@ fn can_loopback_time_sync() {
     )
     .unwrap();
 
+    let clock_handle = StubClockHandle::new();
     let node_id = CanNodeId::try_from(3_u8).unwrap();
     let mut node: CoreNode<
-        SystemClock,
+        StubClock<'_>,
         CanTransmitter<Microseconds64, LoopbackOnlyDriver>,
         CanReceiver<Microseconds64, LoopbackOnlyDriver>,
         TransferIdFixedMap<CanTransport, 4>,
@@ -45,7 +45,7 @@ fn can_loopback_time_sync() {
         4,
         4,
     > = CoreNode::new(
-        SystemClock::new(),
+        clock_handle.clock(),
         node_id,
         CanTransmitter::new(Mtu::Can8),
         CanReceiver::new(node_id, Mtu::Can8),
@@ -63,6 +63,7 @@ fn can_loopback_time_sync() {
             Priority::Nominal,
         )
         .unwrap();
+    clock_handle.set_time(10);
     // Send a non-loopback transfer, which should be ignored
     node.publish(
         &sync_token,
@@ -73,16 +74,24 @@ fn can_loopback_time_sync() {
     .unwrap();
 
     let mut collector = LoopbackCollector::default();
+    clock_handle.set_time(20);
     node.receive(&mut collector)
         .expect("Unexpected error in receive");
     assert_eq!(0, collector.transfers.len());
 
     // Send a loopback transfer, which should be collected
+    // Outgoing frames will have their transmit deadline (timestamp) set to 30 microseconds
+    // + 100 milliseconds.
+    // The frames will actually get to the driver at a time of 30 microseconds, so the timestamp
+    // of the received loopback transfer should match that.
+    clock_handle.set_time(30);
     let loopback_payload = Synchronization {
         previous_transmission_timestamp_microsecond: 129,
     };
     node.publish_loopback(&sync_token, &loopback_payload)
         .unwrap();
+    // Simulate a short delay before receiving
+    clock_handle.set_time(40);
     node.receive(&mut collector)
         .expect("Unexpected error in receive");
     assert_eq!(1, collector.transfers.len());
@@ -90,6 +99,10 @@ fn can_loopback_time_sync() {
     assert_eq!(received_loopback.loopback, true);
     assert_eq!(received_loopback.header.priority(), &Priority::Nominal);
     assert_eq!(received_loopback.header.source(), Some(&node_id));
+    assert_eq!(
+        received_loopback.header.timestamp(),
+        Microseconds64::new(30)
+    );
     let loopback_deserialized_payload =
         Synchronization::deserialize_from_bytes(&received_loopback.payload).unwrap();
     assert_eq!(
@@ -207,5 +220,35 @@ impl TransferHandler<Microseconds64, CanTransport> for LoopbackCollector {
     ) -> bool {
         self.transfers.push(transfer.clone());
         true
+    }
+}
+
+struct StubClock<'t> {
+    time: &'t Cell<u64>,
+}
+
+impl Clock for StubClock<'_> {
+    type Instant = Microseconds64;
+
+    fn now(&mut self) -> Self::Instant {
+        Microseconds64::new(self.time.get())
+    }
+}
+
+struct StubClockHandle {
+    time: Cell<u64>,
+}
+
+impl StubClockHandle {
+    pub fn new() -> Self {
+        StubClockHandle { time: Cell::new(0) }
+    }
+    /// Sets the time that all associated stub clocks will return
+    pub fn set_time(&self, time: u64) {
+        self.time.set(time);
+    }
+    /// Creates and returns a stub clock that always returns the same time as this handle
+    pub fn clock(&self) -> StubClock<'_> {
+        StubClock { time: &self.time }
     }
 }
